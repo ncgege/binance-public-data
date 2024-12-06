5d88a257101110c89b7e06564496b4abcd8a4d1f
ncgege <8160903@qq.com>
2024-12-06 18:14:02 +0800

    修改打印使用方式

--- python/download_daily_kline.py
+++ python/download_daily_kline.py
@@ -6,12 +6,12 @@ from utility import get_parser
 
 if __name__ == "__main__":
     parser = get_parser('klines')
-    args = parser.parse_args(sys.argv[1:])
-    if len(args) < 8:
+    if len(sys.arv) < 8:
         print("""
         参数个数{}少于指定个数8，程序使用方式：
         python3 download_daily_kline.py -t um -s XRPUSDT  -i 1d -skip-monthly 1 -startDate 2024-10-01 -endDate 2024-12-06
-        """.format(len(args)))
+        """.format(len(sys.arv)))
+    args = parser.parse_args(sys.argv[1:])
     downloader = KlineDownloader(args)
     print(args)
     downloader.do_download_daily_klines()
