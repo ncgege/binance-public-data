7261a7eaf0a044259e50b1e8fc8a7884a24046f0
ncgege <8160903@qq.com>
2024-12-06 18:06:15 +0800

    检测参数个数

--- python/downlaod_daily_kline.py
+++ python/downlaod_daily_kline.py
@@ -7,6 +7,11 @@ from utility import get_parser
 if __name__ == "__main__":
     parser = get_parser('klines')
     args = parser.parse_args(sys.argv[1:])
+    if len(args) < 8:
+        print("""
+        参数个数{}少于指定个数8，程序使用方式：
+        python3 download_daily_kline.py -t um -s XRPUSDT  -i 1d -skip-monthly 1 -startDate 2024-10-01 -endDate 2024-12-06
+        """.format(len(args)))
     downloader = KlineDownloader(args)
     print(args)
     downloader.do_download_daily_klines()
