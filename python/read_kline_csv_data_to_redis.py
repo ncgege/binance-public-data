e3522c3d52bfe2fa9d14e747c0ed59bdc60db198
ncgege <8160903@qq.com>
2024-12-16 12:16:51 +0800

    排除已有数据

--- python/read_kline_csv_data_to_redis.py
+++ python/read_kline_csv_data_to_redis.py
@@ -21,21 +21,27 @@ if not os.path.exists(data_dir):
 # 并且保证原来数据+写入数据不超过KLINE_KEEP_COUNT = 50条
 file_list = os.listdir(data_dir)
 symbols = [f.split('-')[0] for f in file_list]
+redis_host = 'localhost'
+redis_port = 6380
+redis_db = 0
+r = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db, decode_responses=True)
+r2 = redis.StrictRedis(host=redis_host, port=6379, db=redis_db, decode_responses=True)
 for symbol in symbols:
     tmp_file_list = [f for f in file_list if f.find(symbol) >= 0]
-    tmp_data = []
-    kline_key = ""
+    interval = tmp_file_list[-1].split('-')[1]
+    # 构造Redis键名
+    kline_key = f"k_list:{symbol}:{interval}:test"
+    cur_kline_list = r.lrange(kline_key, 0, -1)
+    cur_kline_list = [json.loads(kd) for kd in cur_kline_list]
+    cur_timestamp_list = [t for t in cur_kline_list]
+    min_timestamp = min(cur_timestamp_list)
+    read_data = []
     for filename in tmp_file_list:
         if filename.endswith('.csv'):
             csv_file_path = os.path.join(data_dir, filename)
             df = pd.read_csv(csv_file_path)
-            interval = csv_file_path.split('/')[-1].split('-')[1]
             # 连接到Redis服务器
-            redis_host = 'localhost'
-            redis_port = 6380
-            redis_db = 0
-            r = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db, decode_responses=True)
-            r2 = redis.StrictRedis(host=redis_host, port=6379, db=redis_db, decode_responses=True)
+
             # 遍历DataFrame并存储数据到Redis
             for index, row in df.iterrows():
                 open_time = row['open_time']
@@ -45,20 +51,16 @@ for symbol in symbols:
                 _open = float(row['open'])
                 high = float(row['high'])
                 low = float(row['low'])
-
-                # 构造Redis键名
-                kline_key = f"k_list:{symbol}:{interval}:test"
-                j_data = {'t': open_time_ms, 'o': _open, 'h': high, 'l': low, "c": close}
-                # 存储close值到Redis
-                tmp_data.append(j_data)
+                if open_time_ms < min_timestamp:
+                    j_data = {'t': open_time_ms, 'o': _open, 'h': high, 'l': low, "c": close}
+                    # 存储close值到Redis
+                    read_data.append(j_data)
 
                 # 打印存储的信息（可选）
     print(kline_key)
-    cur_kline_list = r.lrange(kline_key, 0, -1)
-    cur_kline_list = [json.loads(kd) for kd in cur_kline_list]
-    tmp_data = sorted(tmp_data, key=lambda x: x['t'], reverse=True)
-    if len(tmp_data) - len(cur_kline_list) >= KLINE_KEEP_COUNT:
-        tmp_data = tmp_data[len(tmp_data) - KLINE_KEEP_COUNT:]
-    tmp_data = [json.dumps(td) for td in tmp_data]
-    r2.lpush(kline_key, *tmp_data)
+    read_data = sorted(read_data, key=lambda x: x['t'], reverse=True)
+    if len(read_data) - len(cur_kline_list) >= KLINE_KEEP_COUNT:
+        read_data = read_data[len(read_data) - KLINE_KEEP_COUNT:]
+    read_data = [json.dumps(td) for td in read_data]
+    r2.lpush(kline_key, *read_data)
     exit()
