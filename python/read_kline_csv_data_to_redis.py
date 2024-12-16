3016194dbf470cf5574714a809d88db7dc95e1e2
ncgege <8160903@qq.com>
2024-12-16 13:17:21 +0800

    排除，数据大于50个原因

--- python/read_kline_csv_data_to_redis.py
+++ python/read_kline_csv_data_to_redis.py
@@ -60,9 +60,10 @@ for symbol in symbols:
     print(f"当前键：{kline_key}， 数据最小时间戳：{min_timestamp}")
     read_data = sorted(read_data, key=lambda x: x['t'], reverse=True)
     print(read_data)
+    print(len(read_data) + len(cur_kline_list))
     if len(read_data) + len(cur_kline_list) >= KLINE_KEEP_COUNT:
-        read_data = read_data[:len(read_data) - KLINE_KEEP_COUNT]
-    print(read_data)
+        read_data = read_data[:KLINE_KEEP_COUNT - len(cur_kline_list)]
+    print('\n\n', read_data)
     read_data = [json.dumps(td) for td in read_data]
     if read_data:
         r2.lpush(kline_key + ":test", *read_data)
